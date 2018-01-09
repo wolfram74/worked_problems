88018c6adbf36504a7be2c8c868862a19d66599d
wolfram74 <phaugen@gmail.com>
2018-01-09 09:12:46 -0700

    updated templater to generate files appropriately

--- docs/templater.py
+++ docs/templater.py
@@ -1,5 +1,5 @@
 '''
-templater.py week_num
+templater.py yyyy_mm_dd num
 problem template
   location
   description section
@@ -9,12 +9,20 @@ problem template
 '''
 import sys
 import os
+import datetime
 
-week_num = int(sys.argv[-1])
+date_str = sys.argv[-2]
+date_list = date_str.split('_')
+print(date_list)
+date_ints = [int(val) for val in date_list]
+start_date = datetime.date(
+  year=int(date_ints[0]),
+  month=int(date_ints[1]),
+  day=int(date_ints[2]))
+one_week = datetime.timedelta(7)
+weeks_to_gen = int(sys.argv[-1])
 problem_goal = 3
 step_budget = 15
-
-print(week_num)
 
 problem_template = '''
   {
@@ -32,10 +40,10 @@ step_template = '''
 view_template = '''
 <html>
 <head>
-  <title>worked problems week %02d</title>
+  <title>worked problems week %s</title>
 </head>
 <body>
-  <h2>worked problems week %02d</h2>
+  <h2>worked problems week %s</h2>
   <div>
     <div id='mainList'>
       <problem-list></problem-list>
@@ -55,14 +63,21 @@ view_template = '''
 
 '''
 
-folder = "week_%02d" % week_num
-os.mkdir(folder)
-view = open(folder+'/view.html', 'w')
-view.write(view_template % (week_num, week_num))
-view.close()
-problems = open(folder+'/problems.js', 'w')
-problems.write('var problems = [\n')
-for prb_num in range(problem_goal):
-    problems.write(problem_template % (step_template*step_budget))
-problems.write(']')
-problems.close()
+def week_gen(monday_date):
+  folder = "week_%s" % monday_date
+  os.mkdir(folder)
+  view = open(folder+'/view.html', 'w')
+  view.write(view_template % (monday_date, monday_date))
+  view.close()
+  problems = open(folder+'/problems.js', 'w')
+  problems.write('var problems = [\n')
+  for prb_num in range(problem_goal):
+      problems.write(problem_template % (step_template*step_budget))
+  problems.write(']')
+  problems.close()
+
+for loop in range(weeks_to_gen):
+  date = start_date+loop*one_week
+  file_date = "%04d_%02d_%02d" % (date.year, date.month, date.day)
+  print(file_date)
+  week_gen(file_date)
