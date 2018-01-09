b5ff36e7b62748a97ee69fe0d3be8b93e6dd1717
wolfram74 <phaugen@gmail.com>
2018-01-09 09:22:39 -0700

    started tracking generator

---
+++ docs/spring_18/indexer.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+folders = os.listdir('.')
+print(folders)
+link_template = '''
+    <li>
+        <a href='./%s/view.html'>week of %s</a>
+    </li>
+'''
+
+index_template = '''
+<html>
+<head>
+  <title>worked problems for %s</title>
+</head>
+<body>
+  <ol>
+%s
+  </ol>
+</body>
+</html>
+'''
+links = []
+for folder in folders:
+    if 'week' not in folder:
+        continue
+    print(folder)
+    # print(link_template% (folder, folder))
+    links.append(link_template% (folder, folder))
+ol_contents = ''.join(links)
+cwd = os.getcwd().split('/')[-1]
+print(index_template % (cwd, ol_contents))
