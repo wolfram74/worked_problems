315434b4990c12b533b74094b64dde0e933c7dd7
wolfram74 <phaugen@gmail.com>
2018-01-16 14:15:55 -0700

    surveyed problems in text and started one

--- docs/spring_18/week_2018_01_15/problems.js
+++ docs/spring_18/week_2018_01_15/problems.js
@@ -1,17 +1,28 @@
 /*
-chen
-ch1: 5 7 9 10
-ch2: 3 7 8 10 11 13
+chen 2nd ed 1984
+ch1:
+  5 shielding distance for a grid
+  7 plug and chug for lambda D
+  9 plug and chug
+  10 derive potential around conductor
+ch2:
+  3 plug and chug to find necessary E for particular EXB drift
+  7 derive ExB drift for electron beam
+  8 calculate some properties of radiation belt
+  10 quick plug and chug for larmour radius
+  11 loss cone fraction calculation
+  13 references problem 12 on page 35
+    calculate escape time for a squeezing magnetic bottle
 */
 var problems = [
 
   {
-    location:`NA`,
-    description:"NA",
+    location:`chen 2nd edition 1-5`,
+    description:"Supposing an infinite conducting screen where the particles are are in a boltzmann distribution $&n_j=n_0 exp(\\frac{-q_j \\phi}{\\tau_j})&$, find the shielding rate that the potential decays at.",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `\\nabla^2\\phi = \\frac{\\rho}{\\epsilon_0}`,
+      explanation: "poisson's equation for electric potential"
     },
     {
       expression: `NA`,
