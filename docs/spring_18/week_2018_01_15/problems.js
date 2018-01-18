4d13e796ae879e02a3a89a59babb2a0370435eea
wolfram74 <phaugen@gmail.com>
2018-01-17 18:34:37 -0700

    some progress on plasma problems

--- docs/spring_18/week_2018_01_15/problems.js
+++ docs/spring_18/week_2018_01_15/problems.js
@@ -25,16 +25,48 @@ var problems = [
       explanation: "poisson's equation for electric potential"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `\\rho = q(n_i-n_e) = q n_0 (exp(\\frac{-q \\phi}{\\tau_i}) - exp(\\frac{+q \\phi}{\\tau_e}))`,
+      explanation: "Using our assumptions about equilibrium to recast charge density in terms of potential"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\rho =
+      q n_0 (
+        \\Sigma_n (\\frac{-q \\phi}{\\tau_i})^n\\frac{1}{n!}
+        - \\Sigma_n (\\frac{q \\phi}{\\tau_e})^n\\frac{1}{n!}
+        )
+      \\approx
+      q n_0 (
+        \\frac{-q \\phi}{\\tau_i}
+        + \\frac{-q \\phi}{\\tau_e}
+        )
+      =
+      -q^2 n_0 (
+        \\frac{1}{\\tau_i}
+        + \\frac{1}{\\tau_e}
+        )\\phi
+      `,
+      explanation: "Taylor expanding charge density. The approximation holds if the potential energy terms are small compared to the kinetic energy terms"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\partial_x^2\\phi
+      =
+      -
+      \\frac{q^2 n_0}{\\epsilon_0} (
+        \\frac{1}{\\tau_i}
+        + \\frac{1}{\\tau_e}
+        )\\phi
+      \\\\
+      \\partial_x^2\\phi
+      +
+      \\frac{q^2 n_0}{\\epsilon_0} (
+        \\frac{1}{\\tau_i}
+        + \\frac{1}{\\tau_e}
+        )\\phi
+      =0
+      `,
+      explanation: "some goof in sign as this implies oscillating solutions, not decaying ones."
     },
     {
       expression: `NA`,
@@ -83,19 +115,26 @@ var problems = [
   },
 
   {
-    location:`NA`,
-    description:"NA",
+    location:`chen 1984 problem 2-7 page 26`,
+    description:"A beam of electrons with a density of 1E14 per cubic meter radius of 1cm from along a uniform B field of 2T in the direction designated as z+, find E field at the surface and corresponding EXB drift",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\nabla \\cdot \\vec E = \\frac{\\rho}{\\epsilon_0}
+      \\int_S dR^2 \\vec E \\cdot \\hat n  = \\int_V dR^3 \\frac{\\rho}{\\epsilon_0}
+      `,
+      explanation: "Gauss's law in integral form."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `\\vec E = 0 \\hat z + E(r) \\hat r + 0 \\hat \\theta`,
+      explanation: "An assumption about what the E field will look like as a result of the geometry of the charge distribution"
     },
     {
-      expression: `NA`,
+      expression: `
+      r < a
+      \\rightarrow E*2\\pi r z = \\frac{\\rho}{\\epsilon_0} \\pi r^2 z
+      \\rightarrow E(r) = \\frac{\\rho}{2 \\epsilon_0}r
+      `,
       explanation: "NA"
     },
     {
