420ddee0932840b446cf90a5232e04179a25e891
wolfram74 <phaugen@gmail.com>
2018-01-23 17:28:52 -0700

    floating point bull shit

--- docs/spring_18/week_2018_01_22/problems.js
+++ docs/spring_18/week_2018_01_22/problems.js
@@ -5,7 +5,7 @@ var problems = [
     description:"take 3 binary strings and convert to 32-bit floating point of the form s-M23-e8 with a bias of 128",
     steps:[
     {
-      expression: `0 00000010 01000000000000000000000`,
+      expression: `1a= 0 00000010 01000000000000000000000`,
       explanation: "raw string"
     },
     // "00000001001000000000000000000000"
@@ -23,11 +23,28 @@ var problems = [
     },
     {
       expression: `2^{-1-126}`,
-      explanation: "is approximately 5.87E-39, compiling string in python"
+      explanation: "is approximately 5.87E-39, compiling string in python produces ~2.94E-38 which differs by a factor of ~5"
     },
     {
-      expression: `1 00000110 00101000000000000000000`,
-      explanation: "NA"
+      expression: `1b= 1 00000110 00101000000000000000000`,
+      explanation: "2nd string"
+    },
+    {
+      expression: `s=1\\rightarrow (-1)^1=-1`,
+      explanation: "It's a negative number"
+    },
+    {
+      expression: `e=00000110=4+2=6\\rightarrow 6-128=-122`,
+      explanation: "the exponent is -122"
+    },
+    {
+      expression: `M = 00101000000000000000000= 0*2^0+0*2^{-1}+1*2^{-2}+0*2^{-3}+1*2^{-4}+0
+      \\\\
+      M=2^{-2} + 2^{-4}=5*2^{-4}
+      \\\\
+      1b = -5*2^{-4-122} = -5*2^{-126}
+      `,
+      explanation: "the final value is approximately -5.87E-38, compiling string in python produces ~(-4.35E-37) which differs by a factor of ~7"
     },
     {
       expression: `0 00100000 01000100000000000000000`,
@@ -84,19 +101,27 @@ var problems = [
   },
 
   {
-    location:`NA`,
-    description:"NA",
+    location:`numerical methods assignment`,
+    description:"convert 329.390625 to a 32-bit floating point number",
     steps:[
     {
-      expression: `NA`,
+      expression: `
+      329 = 101001001
+      \\\\
+      0.390625 = 0.011001
+      `,
+      explanation: "binary expressions provided for the number"
+    },
+    {
+      expression: `
+      101001001.011001=1.01001001011001*2^{8}
+      \\rightarrow
+      M = 01001001011001
+      `,
       explanation: "NA"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
+      expression: `e-E=8=e-128\\rightarrow e=136=128+8=2^7+2^3`,
       explanation: "NA"
     },
     {
--- docs/spring_18/week_2018_01_22/scratch.py
+++ docs/spring_18/week_2018_01_22/scratch.py
@@ -2,7 +2,7 @@ def binToFloat(binstr):
     #based on https://stackoverflow.com/questions/33483846/how-to-convert-32-bit-binary-to-float
     #to check approach
     import struct
-    print struct.unpack(
+    floatie = struct.unpack(
         'f',
         struct.pack(
             'I',
@@ -11,9 +11,15 @@ def binToFloat(binstr):
                 )
             )
         )[0]
+    print floatie
+    return floatie
 
 def prob1Check():
+    t1 = "00000001001000000000000000000000"
+    t1n = "00000000110000000000000000000000"
     binToFloat("00000001001000000000000000000000")
+    binToFloat("00000000110000000000000000000000")
+    print(binToFloat(t1)/binToFloat(t1n))
     binToFloat("10000011000101000000000000000000")
     binToFloat("00010000001000100000000000000000")
 
