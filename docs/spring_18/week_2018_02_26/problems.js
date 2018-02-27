9b6c26fcd4650159e4f0d4e27775edc831e70e34
wolfram74 <phaugen@gmail.com>
2018-02-27 14:50:02 -0700

    wrote up hw3

--- docs/spring_18/week_2018_02_26/problems.js
+++ docs/spring_18/week_2018_02_26/problems.js
@@ -45,7 +45,7 @@ thoughts on problem 3
       \\\\
       E(x)=e^{\\xi}*(.2)(.1)(-.1)/6=e^{\\xi}*(-3.3*10^{-4})
       \\\\
-      E_min =(-3.3*10^{-4}), E_max =(-4.5*10^{-4})
+      E_{min} =(-3.3*10^{-4}), E_{max} =(-4.5*10^{-4})
       `,
       explanation: "1.2218 vs 1.2214"
     },
@@ -105,11 +105,14 @@ thoughts on problem 3
     steps:[
     {
       expression: `.`,
-      explanation: "NA"
+      explanation: "to run on a *nix machine, go to your shell and enter the command 'make -version', if something is returned then while in the directory 'neville_algo' the command 'make clean; make; ./use_neville.ken' should execute the relevant built program."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `f(x)-P_n(x) = \\frac{1}{(n+1)!}f^{n+1}(\\xi) \\Pi_{i=0}^n(x-x_i)=
+      \\\\
+      \\frac{1}{(n+1)!}f^{n+1}(\\xi) \\Pi_{i=0}^n(x-x_i)
+      `,
+      explanation: "The estimated errors in our case agree for $&5\\pi/4&$ fairly well, but Cn and the error estimate formula disagree for the point $&\\pi/4&$."
     },
     {
       expression: `NA`,
@@ -167,11 +170,15 @@ thoughts on problem 3
 
   {
     location:`from Numerical Methods handout`,
-    description:"NA",
+    description:"Implement Kaczmarz algorithm for finding solutions to under-defined equations",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\vec \\delta = \\vec s-\\vec g
+      \\\\
+      error=\\frac{2*|\\delta|}{|s|+|g|}
+      `,
+      explanation: "the procedure for running kaczmarz is similar to running neville's. Kaczmarz algorithm produces a vector which when put through the matrix in question should produce a vector very close to the constraints in question [$&\\vec s&$], the approximation is referred to as a guess vector g. an error is defined in terms of the delta between the guessed and the actual constraints. In the appendix is included a graph where the error is plotted as a function of iterations of the algorithm run. As the slope of the log-log plot appears to be positive, we infer that the algorithm converges super-polynomially."
     },
     {
       expression: `NA`,
