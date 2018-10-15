56ca532cd9c04f7efee49c59df1789db75fda28c
wolfram74 <phaugen@gmail.com>
2018-10-15 13:48:18 -0600

    wrapped for pset 5

--- docs/fall_18/week_2018_10_08/problems.js
+++ docs/fall_18/week_2018_10_08/problems.js
@@ -6,24 +6,78 @@ var problems = [
     description:"Find the 2nd order perturbation for a bead along a paraboloid starting with the 0th and 1st order solutions form notes.",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      0=\\ddot r + 2 \\alpha \\lambda r - \\omega^{2} r
+      \\\\
+      0=\\ddot z + g - \\lambda
+      \\\\
+      0=L-m \\omega r^{2}
+      \\\\
+      0=z- \\alpha r^{2}
+      `,
+      explanation: "The equations of motion for our system."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      r= r_0 + \\epsilon r_1 + \\epsilon^2 r_2
+      \\\\
+      z= z_0 + \\epsilon z_1 + \\epsilon^2 z_2
+      \\\\
+      \\omega= \\omega_0 + \\epsilon \\omega_1 + \\epsilon^2 \\omega_2
+      \\\\
+      \\lambda= \\lambda_0 + \\epsilon \\lambda_1 + \\epsilon^2 \\lambda_2
+      `,
+      explanation: "assuming our solution has some equilibrium motion and higher order perturbations"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      0= 2 \\alpha \\left(\\epsilon^{2} \\operatorname{\\lambda_{2}}{\\left (t \\right )} + \\epsilon \\operatorname{\\lambda_{1}}{\\left (t \\right )} + \\lambda_{0}\\right) \\left(\\epsilon^{2} \\operatorname{r_{2}}{\\left (t \\right )} + \\epsilon \\operatorname{r_{1}}{\\left (t \\right )} + r_{0}\\right)
+      \\\\
+      + \\epsilon^{2} \\frac{d^{2}}{d t^{2}}  \\operatorname{r_{2}}{\\left (t \\right )} + \\epsilon \\frac{d^{2}}{d t^{2}}  \\operatorname{r_{1}}{\\left (t \\right )} - \\left(\\epsilon^{2} \\omega_{2}{\\left (t \\right )} + \\epsilon \\omega_{1}{\\left (t \\right )} + \\omega_{0}\\right)^{2} \\left(\\epsilon^{2} \\operatorname{r_{2}}{\\left (t \\right )} + \\epsilon \\operatorname{r_{1}}{\\left (t \\right )} + r_{0}\\right)
+      \\\\
+      =
+      2 \\alpha \\lambda_{0} r_{0} - \\omega_{0}^{2} r_{0}
+      \\\\
+      + \\epsilon \\left(2 \\alpha \\lambda_{0} \\operatorname{r_{1}}{\\left (t \\right )} + 2 \\alpha r_{0} \\operatorname{\\lambda_{1}}{\\left (t \\right )} - \\omega_{0}^{2} \\operatorname{r_{1}}{\\left (t \\right )} - 2 \\omega_{0} r_{0} \\omega_{1}{\\left (t \\right )} + \\frac{d^{2}}{d t^{2}}  \\operatorname{r_{1}}{\\left (t \\right )}\\right)
+      \\\\
+      + \\epsilon^{2} \\left(2 \\alpha \\lambda_{0} \\operatorname{r_{2}}{\\left (t \\right )} + 2 \\alpha r_{0} \\operatorname{\\lambda_{2}}{\\left (t \\right )} + 2 \\alpha \\operatorname{\\lambda_{1}}{\\left (t \\right )} \\operatorname{r_{1}}{\\left (t \\right )} - \\omega_{0}^{2} \\operatorname{r_{2}}{\\left (t \\right )} - 2 \\omega_{0} r_{0} \\omega_{2}{\\left (t \\right )} - 2 \\omega_{0} \\omega_{1}{\\left (t \\right )} \\operatorname{r_{1}}{\\left (t \\right )} - r_{0} \\omega_{1}^{2}{\\left (t \\right )} + \\frac{d^{2}}{d t^{2}}  \\operatorname{r_{2}}{\\left (t \\right )}\\right)
+      + O(\\epsilon^3)
+      `,
+      explanation: "we can take the 2nd order term to further refine the variation in r."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      0=- \\epsilon^{2} \\lambda_{2}{\\left (t \\right )} + \\epsilon^{2} \\frac{d^{2}}{d t^{2}}  \\operatorname{z_{2}}{\\left (t \\right )} - \\epsilon \\lambda_{1}{\\left (t \\right )} + \\epsilon \\frac{d^{2}}{d t^{2}}  \\operatorname{z_{1}}{\\left (t \\right )} + g - \\lambda_{0}
+      \\\\
+      =
+      g - \\lambda_{0}
+      + \\epsilon \\left(- \\lambda_{1}{\\left (t \\right )} + \\frac{d^{2}}{d t^{2}}  \\operatorname{z_{1}}{\\left (t \\right )}\\right)
+      + \\epsilon^{2} \\left(- \\lambda_{2}{\\left (t \\right )} + \\frac{d^{2}}{d t^{2}}  \\operatorname{z_{2}}{\\left (t \\right )}\\right)
+      \\\\
+      0=- \\alpha \\left(\\epsilon^{2} \\operatorname{r_{2}}{\\left (t \\right )} + \\epsilon \\operatorname{r_{1}}{\\left (t \\right )} + r_{0}\\right)^{2} + \\epsilon^{2} \\operatorname{z_{2}}{\\left (t \\right )} + \\epsilon \\operatorname{z_{1}}{\\left (t \\right )} + z_{0}
+      \\\\
+      =
+      - \\alpha r_{0}^{2} + z_{0}
+      + \\epsilon \\left(- 2 \\alpha r_{0} \\operatorname{r_{1}}{\\left (t \\right )} + \\operatorname{z_{1}}{\\left (t \\right )}\\right)
+      + \\epsilon^{2} \\left(- 2 \\alpha r_{0} \\operatorname{r_{2}}{\\left (t \\right )} - \\alpha \\operatorname{r_{1}}^{2}{\\left (t \\right )} + \\operatorname{z_{2}}{\\left (t \\right )}\\right)
+      + O(\\epsilon^3)
+      `,
+      explanation: "a similar process for the simpler z and constraint equations"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      0=
+      L - m \\left(\\epsilon^{2} \\omega_{2}{\\left (t \\right )} + \\epsilon \\omega_{1}{\\left (t \\right )} + \\omega_{0}\\right) \\left(\\epsilon^{2} \\operatorname{r_{2}}{\\left (t \\right )} + \\epsilon \\operatorname{r_{1}}{\\left (t \\right )} + r_{0}\\right)^{2}
+      \\\\
+      =
+      L - m \\omega_{0} r_{0}^{2}
+      \\\\
+      + \\epsilon \\left(- 2 m \\omega_{0} r_{0} \\operatorname{r_{1}}{\\left (t \\right )} - m r_{0}^{2} \\omega_{1}{\\left (t \\right )}\\right)
+      \\\\
+      + \\epsilon^{2} \\left(- 2 m \\omega_{0} r_{0} \\operatorname{r_{2}}{\\left (t \\right )} - m \\omega_{0} \\operatorname{r_{1}}^{2}{\\left (t \\right )} - m r_{0}^{2} \\omega_{2}{\\left (t \\right )} - 2 m r_{0} \\omega_{1}{\\left (t \\right )} \\operatorname{r_{1}}{\\left (t \\right )}\\right)
+      + O(\\epsilon^3)
+      `,
+      explanation: "Producing the equations of motion for angular frequency to 3rd order. In all these expansions we have 0th and first order terms that already reduce to 0."
     },
     {
       expression: `NA`,
@@ -192,12 +246,31 @@ var problems = [
     description:"Show that the potential $&V(r) = -kr^{-1}+hr^{-2}&$ is equivalent to the keplerian potential in a rotational reference frame. Show that for bound orbits the rotating frame period is $&\\frac{2\\pi m h }{l^2 \\tau}&$ ",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\mathcal{L}
+      = m\\dot r^2 + m r^2\\dot \\theta +kr^{-1}-hr^{-2}
+      \\\\
+      L = m r^2 \\dot theta
+      \\\\
+      \\mathcal{L}
+      = m\\dot r^2 - (L/m) r^{-2} +kr^{-1}-hr^{-2}
+      `,
+      explanation: "recasting the equation just in terms of r"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      - (L/m) r^{-2} -hr^{-2} = -(L/m+h)r^{-2}
+      \\\\
+      L/m+h = L_f/m
+      \\\\
+      L_f = r^2 \\dot \\theta_f
+      \\rightarrow
+      (L/m+h)r^{-2} = \\dot \\theta_f  > \\dot \\theta
+      \\\\
+      \\mathcal{L}
+      = m\\dot r^2 - L_f r^{-2} +kr^{-1}
+      `,
+      explanation: "taking advantage of the similar forms of the imposed potential and the virtual potential imposed by angular momentum, we see for positive perturbations the orbit is always faster than it would have been without."
     },
     {
       expression: `NA`,
@@ -266,11 +339,34 @@ var problems = [
       explanation: "The vector form of the equations of motion for this system."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `\\vec D = \\vec r \\times \\vec p - \\frac{qb}{c}\\hat r`,
+      explanation: "define a composite vector from the angular momentum and charge"
     },
     {
-      expression: `NA`,
+      expression: `
+      \\dot{\\vec {D}}
+      =
+      \\dot{\\vec r} \\times \\vec p
+      +
+      \\vec r \\times \\dot{\\vec p}
+      - \\frac{qb}{c}\\dot{ \\hat{ r}}
+      \\\\
+      \\dot{\\vec r} \\times \\vec p =
+      \\dot{\\vec r} \\times \\dot{\\vec r}m = 0
+      \\\\
+      \\vec r \\times \\dot{\\vec p}=
+      \\vec r \\times (
+        q(\\vec E + \\dot{\\vec r}\\times \\vec B )
+        )
+      \\\\
+      \\vec r \\times q \\vec E
+      =
+      -qkr^{-2} \\vec r \\times \\hat r = 0
+      \\\\
+      q \\vec r \\times \\dot{\\vec r}\\times \\vec B
+      =
+      qbr^{-2} \\vec r \\times \\dot{\\vec r}\\times \\hat r
+      `,
       explanation: "NA"
     },
     {
--- docs/fall_18/week_2018_10_08/scratch.py
+++ docs/fall_18/week_2018_10_08/scratch.py
@@ -7,8 +7,45 @@ vprint = sympy.physics.vector.printing.vprint
 def mathjaxify(expr):
     print(sympy.latex(expr).replace('\\', '\\\\'))
 
-def prob1():
+def perturber(expr, sub_vals, ep):
+    pprint(expr)
+    subbed = expr.subs(sub_vals)
+    pprint(subbed)
+    mathjaxify(subbed)
+    pprint(subbed.expand().collect(ep))
+    perturbed = subbed.expand().collect(ep).subs([(ep**n, 0) for n in range(3, 7)])
+    pprint(perturbed)
+    mathjaxify(perturbed)
 
+def prob1():
+    a, az, r, z, w, lam, alpha, m, g = sympy.symbols('a a_z r z omega lambda alpha m g')
+    r_0, z_0, w_0, lam_0, ep, t, L = sympy.symbols('r0 z0 omega_0 lambda_0 epsilon t L')
+    r_1,z_1, w_1, lam_1 = dynamicsymbols('r1 z1 omega_1 lambda_1')
+    r_2,z_2, w_2, lam_2 = dynamicsymbols('r2 z2 omega_2 lambda_2')
+    r_eq = a - r*w**2+2*lam*alpha*r
+    z_eq = az + g - lam
+    ang_eq =L- m*r**2*w
+    lam_eq = z-alpha*r**2
+    # mathjaxify(r_eq)
+    # mathjaxify(z_eq)
+    # mathjaxify(ang_eq)
+    # mathjaxify(lam_eq)
+    r_var = r_0+ep*r_1+ep**2*r_2
+    z_var = z_0+ep*z_1+ep**2*z_2
+    w_var = w_0+ep*w_1+ep**2*w_2
+    lam_var = lam_0+ep*lam_1+ep**2*lam_2
+    sub_vals = [
+        (r, r_var),
+        (a, r_var.diff(t).diff(t)),
+        (z, z_var),
+        (az, z_var.diff(t).diff(t)),
+        (w, w_var),
+        (lam, lam_var),
+        ]
+    # perturber(r_eq, sub_vals, ep)
+    # perturber(z_eq, sub_vals, ep)
+    perturber(ang_eq, sub_vals, ep)
+    # perturber(lam_eq, sub_vals, ep)
     return
 
 def prob2():
@@ -58,4 +95,4 @@ def prob4():
     return
 
 if __name__=='__main__':
-    prob2()
+    prob1()
