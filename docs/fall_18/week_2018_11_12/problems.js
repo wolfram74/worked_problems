5883f46d50168c43be47b4f5d8f31ea3d5bf0670
wolfram74 <phaugen@gmail.com>
2018-11-12 12:51:42 -0700

    described problems for mechanics

--- docs/fall_18/week_2018_11_12/problems.js
+++ docs/fall_18/week_2018_11_12/problems.js
@@ -1,8 +1,140 @@
 var problems = [
-
+/*described*/
   {
-    location:`NA`,
-    description:"NA",
+    location:`problem set 6`,
+    description:"given a mass m=2E3kg, a radius r=6400E3m, an object at $&\\theta_p=\\pi/2&$ traveling towards the equator of the earth which rotates at $&\\Omega=2\\pi/(24*3600)&$, find the centrifugal and coriolis forces that must be counteracted to maintain it's course.",
+    steps:[
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },]
+  },
+/*described*/
+  {
+    location:`Problem set 6`,
+    description:"given a mass density $&\\rho = \\rho_0\\delta(z)&$ for x between 0 and a and y between 0 and b, otherwise 0. Calculate the momenta of inertia for rotation around the z axis",
+    steps:[
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+      },]
+  },
+/*described*/
+  {
+    location:`Problem set 6`,
+    description:"find the moment of inertia tensor for a uniform cylinder about it's center of mass. Describe it's motion in time when the initial angular velocity is mostly along the long axis.",
     steps:[
     {
       expression: `NA`,
@@ -67,78 +199,18 @@ var problems = [
   },
 
   {
-    location:`NA`,
-    description:"NA",
+    /*
+  addendum, sphere of constant mass M and radius R, but density of ar^n
+  therefore a=(n+3)M (4 pi R^(n+3))^-1
+    */
+    location:`Problem set 6`,
+    description:"find the moment of inertia tensor for a sphere around it's center given various density functions",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
-    },]
-  },
-
-  {
-    location:`NA`,
-    description:"NA",
-    steps:[
-    {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\rho = (n+3)M (4 pi R^(n+3))^{-1}r^n
+      `,
+      explanation: "density as a power of radius such that mass of the sphere is constant."
     },
     {
       expression: `NA`,
