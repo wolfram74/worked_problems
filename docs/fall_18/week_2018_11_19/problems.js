b93169f10a497bef034908253ae9e3d28de5f3b0
wolfram74 <phaugen@gmail.com>
2018-11-19 11:39:16 -0600

    prob 3

--- docs/fall_18/week_2018_11_19/problems.js
+++ docs/fall_18/week_2018_11_19/problems.js
@@ -1,8 +1,84 @@
 var problems = [
 
   {
-    location:`NA`,
-    description:"NA",
+    location:`midterm`,
+    description:"find the corresponding maxwell relation for $&\\partial_V \\mu|_{T,N}&$",
+    steps:[
+    {
+      expression: `
+      dF = \\mu dN -SdT-pdV
+      `,
+      explanation: "Free energy differential"
+    },
+    {
+      expression: `
+      \\partial_N F = \\mu
+      \\\\
+      \\partial_V \\partial N F = \\partial_N \\partial_V F
+      \\\\
+      \\partial_V F = -p
+      \\\\
+      \\partial_V \\mu = -\\partial_N p
+      `,
+      explanation: "how chemical potential and pressure relate."
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },]
+  },
+
+  {
+    location:`midterm`,
+    description:"derive the probability relation $&p_i = \\frac{e^{-\\beta E_i}}{Z}&$",
     steps:[
     {
       expression: `NA`,
@@ -131,7 +207,71 @@ var problems = [
       explanation: "NA"
     },]
   },
-
+  {
+    location:`NA`,
+    description:"NA",
+    steps:[
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },
+    {
+      expression: `NA`,
+      explanation: "NA"
+    },]
+  },
   {
     location:`NA`,
     description:"NA",
