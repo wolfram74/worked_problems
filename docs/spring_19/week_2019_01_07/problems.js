ccce50bde71ebcb0c64dcd8c31a4cd0907add0a4
wolfram74 <phaugen@gmail.com>
2019-01-20 14:46:23 -0700

    time constant determined

--- docs/spring_19/week_2019_01_07/problems.js
+++ docs/spring_19/week_2019_01_07/problems.js
@@ -5,19 +5,58 @@ var problems = [
     description:"cast the time constant from the damped oscillations section of stump and pollacks paper on radiation damping in dipoles using the dimensionless coordinates",
     steps:[
     {
-      expression: `\\tau_{decay}=12\\pi \\epsilon_0 c^5 I/\\mu_1^2 \\omega_0^2`,
+      expression: `
+      \\tau_{decay}=12\\pi \\epsilon_0 c^5 I/\\mu_1^2 \\omega_0^2
+      `,
+      explanation: "Time constant from stump modeling the decay as exponential."
+    },
+    {
+      expression: `
+      c^2 = (\\epsilon_0 \\mu_0)^{-1}
+      \\rightarrow
+      \\tau_{decay}
+      =12\\pi \\frac{c^3 I}{\\mu_0 \\mu_1^2 \\omega_0^2}
+      `,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `
+      F_0 =
+      \\frac{3}{2\\pi}
+      \\frac{\\mu_0 \\mu_1^2}{L_0^4}
+      \\\\
+      \\mu_0 \\mu_1^2 =
+      \\frac{2\\pi}{3}
+      F_0 L_0^4
+      \\\\
+      \\tau_{decay}
+      =12\\pi \\frac{3}{2\\pi}
+      \\frac{c^3 I}{F_0 L_0^4 \\omega_0^2}
+      =18
+      \\frac{c^3 I}{F_0 L_0^4 \\omega_0^2}
+      `,
       explanation: "NA"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
+      expression: `
+      I= m_1 L_0^2 10
+      \\\\
+      T_0^2 =\\frac{m_1 L_0}{F_0}
+      \\\\
+      \\tau_{decay}
+      =18
+      \\frac{c^3 m_1 L_0^2 10}{F_0 L_0^4 \\omega_0^2}
+      \\\\
+      \\tau_{decay}
+      =180
+      \\frac{c^3  L_0}{L_0^4 \\omega_0^2} \\frac{m_1 L_0}{ F_)}
+      \\\\
+      \\tau_{decay}
+      =180
+      \\frac{c^3}{L_0^3 \\omega_0^2} T_0^2
+      =180
+      \\frac{c^3}{L_0^2 \\omega_0^2 \\frac{L_0}{T_0}} T_0
+      `,
       explanation: "NA"
     },
     {
