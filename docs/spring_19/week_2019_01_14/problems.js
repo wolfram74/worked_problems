afa204cecaeb994eb69918024e779f63faefae84
wolfram74 <phaugen@gmail.com>
2019-01-23 15:16:35 -0700

    wow, gr, butts

--- docs/spring_19/week_2019_01_14/problems.js
+++ docs/spring_19/week_2019_01_14/problems.js
@@ -1,27 +1,112 @@
 var problems = [
+/*
+steering notes
+  find 4th and 5th member for committee
+  form committee
+  approve program of study
+  candidacy exam topic approved and scheduled for fall
+  topic by committee/advisor
+    format: paper synthesizing content of several related papers and presentation
+  thesis proposal
 
+*/
   {
     location:`NA`,
-    description:"NA",
+    description:"SO(1,3) is the group of transformations $&\\Lambda&$ such that they are in GL(4), have a determinant of 1 and $&\\Lambda^T \\eta \\Lambda=\\eta&$",
     steps:[
+    /*
+    show
+      Xclosure
+      associativity
+      identity
+      invert ability
+    */
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\Lambda_1\\Lambda_2 = \\Lambda_3
+      \\\\
+      |A| |B| = |AB|\\rightarrow |\\Lambda_3|=1
+      \\\\
+      \\Lambda_3^T = \\Lambda_2^T\\Lambda_1^T \\rightarrow
+      \\Lambda_3^T \\eta \\Lambda_3
+      =
+      \\Lambda_2^T\\Lambda_1^T \\eta \\Lambda_1\\Lambda_2
+      =
+      \\Lambda_2^T \\eta \\Lambda_2
+      =
+      \\eta
+      `,
+      explanation: "The determinant continues to be 1 as well as leaving $&\\eta&$ unchanged, so the set is closed under multiplication. $&\\Lambda_i&$ is part of GL(4) and thus are also associative. The identity of $&\\delta_{n,m}&$ exists for 4x4 matrices by inpsection, and"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      det(\\Lambda_{23})=sin(\\theta)^2 + cos(\\theta)^2=1
+      \\\\
+      det(\\Lambda_{02}) = - \\sinh^{2}{\\left (\\phi \\right )} + \\cosh^{2}{\\left (\\phi \\right )} = 1
+      \\\\
+      \\Lambda_{23}^T\\eta\\Lambda_{23}
+      =
+      \\left[\\begin{matrix}-1 & 0 & 0 & 0\\\\0 & 1 & 0 & 0\\\\0 & 0 & \\sin^{2}{\\left (\\theta \\right )} + \\cos^{2}{\\left (\\theta \\right )} & 0\\\\0 & 0 & 0 & \\sin^{2}{\\left (\\theta \\right )} + \\cos^{2}{\\left (\\theta \\right )}\\end{matrix}\\right] =\\eta
+      \\\\
+      \\Lambda_{02}^T\\eta\\Lambda_{02}
+      =
+      \\left[\\begin{matrix}\\sinh^{2}{\\left (\\phi \\right )} - \\cosh^{2}{\\left (\\phi \\right )} & 0 & 0 & 0\\\\0 & 1 & 0 & 0\\\\0 & 0 & - \\sinh^{2}{\\left (\\phi \\right )} + \\cosh^{2}{\\left (\\phi \\right )} & 0\\\\0 & 0 & 0 & 1\\end{matrix}\\right]=\\eta
+      `,
+      explanation: "the determinant is one and returns the metric when applied."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      cos(\\theta) = 1 + O(\\theta^2)
+      \\\\
+      sin(\\theta) = 1 + \\theta + O(\\theta^2)
+      \\\\
+      cosh(\\theta) = 1 + O(\\theta^2)
+      \\\\
+      sinh(\\theta) = 1 + \\theta  + O(\\theta^2)
+      \\\\
+      \\lambda_{23} =
+      \\left[\\begin{matrix}0 & 0 & 0 & 0\\\\0 & 0 & 0 & 0\\\\0 & 0 & 0 & 1\\\\0 & 0 & - 1 & 0\\end{matrix}\\right]
+      ,
+      \\lambda_{02} =
+      \\left[\\begin{matrix}0 & 0 &  1 & 0\\\\0 & 0 & 0 & 0\\\\ 1 & 0 & 0 & 0\\\\0 & 0 & 0 & 0\\end{matrix}\\right]
+      \\\\
+      \\lambda_{23}^T \\eta + \\eta \\lambda_{23} = \\widehat 0
+      \\\\
+      \\lambda_{02}^T \\eta + \\eta \\lambda_{02} = \\widehat 0
+
+      `,
+      explanation: "satisfying the requirements for so(1,3)"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      (\\lambda_{\\mu \\nu})^{\\rho}_{\\ \\sigma}
+      \\equiv
+      \\delta_{\\mu}^{\\ \\rho} \\eta_{\\nu  \\sigma}
+      -\\delta_{\\nu}^{\\ \\rho} \\eta_{\\mu  \\sigma}
+      \\\\
+      (\\lambda_{2 3})^{\\rho}_{\\ \\sigma}
+      \\equiv
+      \\delta_{2}^{\\ \\rho} \\eta_{3  \\sigma}
+      -\\delta_{3}^{\\ \\rho} \\eta_{2  \\sigma}
+      \\rightarrow
+      (\\lambda_{2 3})^{2}_{\\ 3} = 1*1-0*0 =1
+      ,
+      (\\lambda_{2 3})^{3}_{\\ 2} = 0*0-1*1 =-1
+      \\\\
+      (\\lambda_{0 2})^{\\rho}_{\\ \\sigma}
+      \\equiv
+      \\delta_{0}^{\\ \\rho} \\eta_{2  \\sigma}
+      -\\delta_{2}^{\\ \\rho} \\eta_{0  \\sigma}
+      \\rightarrow
+      (\\lambda_{2 3})^{0}_{\\ 2} = 1*1-0*0 =1
+      ,
+      (\\lambda_{2 3})^{2}_{\\ 0} = 0*0-1*(-1) =1
+      `,
+      // - delta_nu^rho eta_{mu sig}
+      explanation: "the excplicitly calculated $&\\lambda&$s can be derived that way."
     },
     {
-      expression: `NA`,
+      expression: `\\lambda_{\\nu \\mu}\\eta ^{\\mu \\alpha} + \\lambda_{\\mu \\nu}\\eta ^{\\mu \\alpha}`,
       explanation: "NA"
     },
     {
@@ -68,47 +153,93 @@ var problems = [
 
   {
     location:`NA`,
-    description:"NA",
+    description:"starting the with 2nd rank tensor $&T^{\\mu \\nu}&$ and first rank tensor $&V^{\\mu}&$ find the components of various transformations",
     steps:[
     {
-      expression: `NA`,
+      expression: `
+      \\eta^{\\alpha \\beta}=\\eta_{\\alpha \\beta}
+      =
+      \\left[\\begin{matrix}-1 & 0 & 0 & 0\\\\0 & 1 & 0 & 0\\\\0 & 0 & 1 & 0\\\\0 & 0 & 0 & 1\\end{matrix}\\right]
+      \\\\
+      T^{\\mu}_{-\\nu} =
+      T^{\\mu \\beta} \\eta_{\\beta \\nu}
+      =
+
+      `,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `T^{-\\nu}_{\\mu}`,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `
+      T^{(\\mu \\nu)}\\equiv (T^{\\mu \\nu}+T^{\\nu \\mu})/2
+      \\\\
+      \\left[\\begin{matrix}
+        2 & 0 & 1 & -1\\\\
+        1 & 0 & -3 & 2\\\\
+        -1 & 1 & 0 & 0\\\\
+        2 & 1 & -1 & 2
+      \\end{matrix}\\right]
+      +
+      \\left[\\begin{matrix}
+        2 & 1 & -1 & 2\\\\
+        0 & 0 & 1 & 1\\\\
+        1 & -3 & 0 & -1\\\\
+        -1 & 2 & 0 & 2
+      \\end{matrix}\\right]
+      =
+      \\left[\\begin{matrix}
+        4 & 1 & 0 & 1\\\\
+        1 & 0 & -2 & 3\\\\
+        0 & -2 & 0 & -1\\\\
+        1 & 3 & -1 & 4
+      \\end{matrix}\\right]
+      \\\\
+      T^{(\\mu \\nu)}
+      =
+      \\left[\\begin{matrix}
+        2 & 1/2 & 0 & 1/2\\\\
+        1/2 & 0 & -1 & 3/2\\\\
+        0 & -1 & 0 & -1/2\\\\
+        1/2 & 3/2 & -1/2 & 2
+      \\end{matrix}\\right]
+
+      `,
+      explanation: "The symmetric part of $&T^{\\mu \\nu}&$"
+    },
+    {
+      expression: `T_{[\\mu \\nu]} \\equiv (T_{\\mu \\nu}-T_{\\nu \\mu})/2`,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `T^{\\mu}_{-\\mu}`,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `
+      V^{\\mu}V_{\\mu}
+      = V^{\\mu} \\eta_{\\alpha \\mu} V^{\\alpha}
+      = (-1)*1+(-2)^2+0^2+2^2=7
+      `,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `V_{\\mu} T^{\\nu \\mu}`,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `T^{\\nu \\mu}V_{\\mu}`,
       explanation: "NA"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `V_{\\nu} T^{\\nu \\mu}`,
+      explanation: "has vector properties"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
-    },
-    {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `V^{\\nu} T_{\\nu \\mu}`,
+      explanation: "has co-vector properties"
     },
     {
       expression: `NA`,
---
+++ docs/spring_19/week_2019_01_14/scratch.py
@@ -0,0 +1,102 @@
+import sympy
+import numpy
+
+cos = sympy.cos
+sin = sympy.sin
+cosh = sympy.cosh
+sinh = sympy.sinh
+
+def mathjaxify(expr):
+    print(sympy.latex(expr).replace('\\', '\\\\'))
+
+def prob1():
+    ph, th = sympy.symbols('phi theta', real=True)
+    L23 = sympy.Matrix([
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,cos(th),sin(th)],
+        [0,0,-sin(th),cos(th)]
+    ])
+    l23 = sympy.Matrix([
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,1],
+        [0,0,-1,0]
+    ])
+    L02 = sympy.Matrix([
+        [cosh(ph),0,sinh(ph),0],
+        [0,1,0,0],
+        [sinh(ph),0,cosh(ph),0],
+        [0,0,0,1],
+    ])
+    l02 = sympy.Matrix([
+        [0,0,1,0],
+        [0,0,0,0],
+        [1,0,0,0],
+        [0,0,0,0],
+    ])
+    eta = sympy.Matrix([
+        [-1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ])
+    # sympy.pprint(L23.det())
+    # sympy.pprint(L02.det())
+    # mathjaxify(L02.det())
+    # sympy.pprint(L23.transpose()*eta*L23)
+    # mathjaxify(L23.transpose()*eta*L23)
+    # sympy.pprint(L02.transpose()*eta*L02)
+    # mathjaxify(L02.transpose()*eta*L02)
+    prob2c(l23,l02, eta)
+
+def prob2c(l23,l02,eta):
+    mathjaxify(l23)
+    mathjaxify(l02)
+    sympy.pprint(l23.transpose()*eta+eta*l23)
+    sympy.pprint(l02.transpose()*eta+eta*l02)
+
+
+def prob2():
+    T = sympy.Matrix([
+        [2,0,1,-1],
+        [1,0,-3,2],
+        [-1,1,0,0],
+        [2,1,-1,2]
+        ])
+    V = sympy.Matrix([1, -2, 0, 2])
+    eta = sympy.Matrix([
+        [-1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,1],
+        ])
+    # sympy.pprint(T)
+    # mathjaxify(eta)
+    # mathjaxify(T)
+    # mathjaxify(T.transpose())
+    # sympy.pprint(T+T.transpose())
+    # sympy.pprint(T*eta)
+    # sympy.pprint(T*eta-eta*T)
+    # sympy.pprint(T*eta-eta*T.transpose())
+    # a_T = numpy.array([
+    #     [2,0,1,-1],
+    #     [1,0,-3,2],
+    #     [-1,1,0,0],
+    #     [2,1,-1,2]
+    #     ])
+    # a_eta = numpy.array([
+    #     [-1,0,0,0],
+    #     [0,1,0,0],
+    #     [0,0,1,0],
+    #     [0,0,0,1],
+    #     ])
+    # res = numpy.einsum('ab,bc->ac', a_T, a_eta)
+    # print(a_T)
+    # print(res)
+    # print(res-a_T)
+
+
+if __name__ =='__main__':
+    prob1()
+    # prob2()
