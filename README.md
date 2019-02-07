ac821da79006a812b9b90dcca90df65233fb62be
wolfram74 <phaugen@gmail.com>
2019-02-07 16:36:48 -0700

    some prelim work on gr

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # worked_problems
 A place to put problems I've worked out for later reference
+
+git add changed_file
+  or
+git add .
+  then
+git commit -m "description of changes"
+
+
+lazy approach
+git commit -am 'commit message'
+
+  to save to remote (dangerous)
+git push origin master
+  more generally
+git push remote_name branch_name
--- docs/spring_19/week_2019_02_04/problems.js
+++ docs/spring_19/week_2019_02_04/problems.js
@@ -2,15 +2,49 @@ var problems = [
 
   {
     location:`NA`,
-    description:"NA",
+    description:"consider the electro-magnetic potential as a set of functions $&A_\\mu&$ and the electro-magnetic field $&F_{\\mu \\nu} = 2(\\partial_\\mu A_\\nu - \\partial_\\nu A_\\mu)&$",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      A_\\mu' = A_\\mu+\\partial_\\mu \\alpha
+      \\\\
+      F_{\\mu \\nu}' =
+      2(\\partial_\\mu A_\\nu' - \\partial_\\nu A_\\mu')
+      = 2(
+        \\partial_\\mu (A_\\nu+\\partial_\\nu \\alpha)
+        - \\partial_\\nu (A_\\mu+\\partial_\\mu \\alpha))
+      \\\\
+      = 2(
+        \\partial_\\mu A_\\nu
+        +\\partial_\\mu \\partial_\\nu \\alpha
+        - \\partial_\\nu A_\\mu
+        - \\partial_\\nu \\partial_\\mu \\alpha)
+      \\\\
+      = 2(
+        \\partial_\\mu A_\\nu
+        - \\partial_\\nu A_\\mu
+        +\\partial_\\mu \\partial_\\nu \\alpha
+        - \\partial_\\nu \\partial_\\mu \\alpha
+        )=F_{\\mu \\nu}+0
+      \\\\
+      `,
+      explanation: "The electro-magnetic field is unchanged by the this transformation."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\epsilon_{ijk} = \\pm 1 (even/odd \\ permutations)
+      \\\\
+      \\epsilon_{imn}\\epsilon^{jmn}=f(i,j,m,n)
+      \\\\
+      i=m, i=n, j=m, j=n \\rightarrow f= 0
+      \\\\
+      i \\neq j \\rightarrow i = (n||m) \\ ||\\  j = (n||m) \\rightarrow f=0
+      \\\\
+      i = j \\rightarrow f= 1-1+1 =1
+      \\\\
+      \\therefore \\epsilon_{imn}\\epsilon^{jmn} = \\delta^{j}_i
+      `,
+      explanation: "pairs of levi civita tensors can compact to delta functions."
     },
     {
       expression: `NA`,
@@ -68,22 +102,36 @@ var problems = [
 
   {
     location:`NA`,
-    description:"NA",
+    description:"suppose two tensors $&T^{\\mu \\nu}&$ and $&S^{\\mu}_{\\ \\nu}&$ show the following quantities are tensors of the indicated rank",
     steps:[
     {
-      expression: `NA`,
+      expression: `
+      A^{\\mu \\nu}
+      =
+      \\phi T^{\\mu \\nu}
+      +\\psi \\eta^{\\alpha \\nu} S^{\\mu}_{\\ \\alpha}`,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `
+      B^{\\mu \\nu \\rho}_{\\ \\ \\ \\ \\ \\sigma}
+      =T^{\\mu \\nu} S^{\\rho}_{\\ \\sigma}
+      `,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `
+      C^{\\mu \\nu}=
+      B^{\\mu  \\rho \\nu}_{\\ \\ \\ \\ \\ \\rho}
+      =T^{\\mu \\rho} S^{\\nu}_{\\ \\rho}
+
+      `,
       explanation: "NA"
     },
     {
-      expression: `NA`,
+      expression: `
+      \\nabla_{\\mu} T^{\\nu \\rho}
+      `,
       explanation: "NA"
     },
     {
