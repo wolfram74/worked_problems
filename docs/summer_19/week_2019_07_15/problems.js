1123221268fc7d7a76d96ddcb0f4ca46644301ed
wolfram74 <phaugen@gmail.com>
2019-07-16 09:00:32 -0500

    write up for circular orbit

--- docs/summer_19/week_2019_07_15/problems.js
+++ docs/summer_19/week_2019_07_15/problems.js
@@ -1,28 +1,48 @@
 var problems = [
 
   {
-    location:`NA`,
-    description:"NA",
+    location:`self`,
+    description:"Describe the conditions for circular orbits of double magnets.",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `\\frac{d}{dt}(\\phi_t - 2 \\theta) = 0`,
+      explanation: "the condition needed for continued no torque on $&p_{\\phi_t}&$ and $&p_\\theta&$"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      20 p_{\\phi_t} - 2\\frac{2 p_{\\theta}}{r^{2}} = 0
+      \\\\
+      p_{\\phi_t} = \\frac{p_{\\theta}}{5 r^{2}}
+      `,
+      explanation: "What must be true to satisfy that earlier stability criterion."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      L_t = p_{\\theta}+2p_{\\phi_t}
+      = p_{\\theta} + \\frac{2 p_{\\theta}}{5 r^{2}}
+      `,
+      explanation: "Considering what the total angular momentum would be in this case."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      \\dot p_r = \\frac{2 p_{\\theta}^{2}}{r^{3}} - \\frac{1}{r^{4}}
+      \\\\
+      p_{\\theta} = \\frac{\\sqrt{2}}{2}(r)^{-1/2}
+      \\\\
+      p_{\\phi_t} = \\frac{\\sqrt{2}}{10}(r)^{-5/2}
+      `,
+      explanation: "the circular orbit values for these momenta."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      L_t =
+      \\frac{\\sqrt{2}}{2}(r)^{-1/2}
+      + \\frac{\\sqrt{2}}{5}(r)^{-5/2}
+      \\\\
+      H_0 =
+      \\frac{1}{6 r^{3}} + \\frac{1}{5 r^{5}}
+      `,
+      explanation: "Conserved quantities under these circumstances. Numerical simulations indicated that while total energy is positive, if slightly below, the system stays confined. If values slightly above, system blows apart."
     },
     {
       expression: `NA`,
