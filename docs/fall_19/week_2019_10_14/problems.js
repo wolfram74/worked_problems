9efb28d70e0bb60e14b6050b01ab39f0bf0e8104
wolfram74 <phaugen@gmail.com>
2019-10-16 12:23:02 -0600

    some geometry work

--- docs/fall_19/week_2019_10_14/problems.js
+++ docs/fall_19/week_2019_10_14/problems.js
@@ -1,19 +1,37 @@
 var problems = [
 
   {
-    location:`NA`,
-    description:"NA",
+    location:`research`,
+    description:"specify the geometry for the filled hexagon. Assumes spheres are diameter $&l&$",
     steps:[
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      U_{i,j}=
+      -\\frac{\\mu_0}{4\\pi}
+      \\frac{\\mu_i \\mu_j}{2}
+      \\frac{1}{r_{i,j}^3}
+      [cos(\\phi_i-\\phi_j)+3cos(\\phi_i+\\phi_j-2\\theta_{i,j})]
+      `,
+      explanation: "potential energy for two spheres, i, j. Central sphere is index 0 and outter 6 range 1 to 6, counter clockwise starting from the +x axis. See diagram at bottom."
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+      r_{i,j}=r_{j,i}
+      \\\\
+      r_{0,i}=l
+      \\\\
+      r_{i-j\\%6=1,5}=l
+      \\\\
+      r_{i-j\\%6=2,4}=3^{1/2}l
+      \\\\
+      r_{i-j\\%6=3}=2l
+      `,
+      explanation: "the radial terms given our symmetry."
     },
     {
-      expression: `NA`,
+      expression: `
+      \\theta{i,j}=\\theta{j,i}
+      `,
       explanation: "NA"
     },
     {
