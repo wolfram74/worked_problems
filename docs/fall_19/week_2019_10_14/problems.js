0166232c2c9de20bed1e56db2c1abc829692bdf3
wolfram74 <phaugen@gmail.com>
2019-10-18 13:36:48 -0500

    including the full kabootle

--- docs/fall_19/week_2019_10_14/problems.js
+++ docs/fall_19/week_2019_10_14/problems.js
@@ -32,7 +32,7 @@ var problems = [
       expression: `
       \\theta_{i,j} \\neq \\theta_{j,i}
       \\\\
-      \\theta_{0,i} = 2 i \\pi/6
+      \\theta_{0,i} = 2 (i-1) \\pi/6
       \\\\
       |\\theta_{i,j}| + |\\theta_{j,i}| = \\pi
       \\\\
@@ -43,8 +43,76 @@ var problems = [
       explanation: "NA"
     },
     {
-      expression: `NA`,
-      explanation: "NA"
+      expression: `
+U_{0,j}, 0
+ \\\\ \\qquad
+, \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{1} \\right )} + 3 \\cos{\\left (\\phi_{0} + \\phi_{1} \\right )}\\right), \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{2} \\right )} - 3 \\cos{\\left (\\phi_{0} + \\phi_{2} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+, \\gamma \\left(- 3 \\sin{\\left (\\phi_{0} + \\phi_{3} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{0} - \\phi_{3} \\right )}\\right), \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{4} \\right )} + 3 \\cos{\\left (\\phi_{0} + \\phi_{4} \\right )}\\right)
+ \\\\ \\qquad
+, \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{5} \\right )} - 3 \\cos{\\left (\\phi_{0} + \\phi_{5} + \\frac{\\pi}{3} \\right )}\\right), \\gamma \\left(- 3 \\sin{\\left (\\phi_{0} + \\phi_{6} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{0} - \\phi_{6} \\right )}\\right)
+ \\\\ \\qquad
+
+\\\\
+U_{1,j}, \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{1} \\right )} - 3 \\cos{\\left (\\phi_{0} + \\phi_{1} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+, 0, \\gamma \\left(- 3 \\sin{\\left (\\phi_{1} + \\phi_{2} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{1} - \\phi_{2} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{1} - \\phi_{3} \\right )} + 3 \\cos{\\left (\\phi_{1} + \\phi_{3} + \\frac{\\pi}{3} \\right )}\\right), \\frac{\\gamma}{8} \\left(\\cos{\\left (\\phi_{1} - \\phi_{4} \\right )} + 3 \\cos{\\left (\\phi_{1} + \\phi_{4} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\sqrt{3} \\gamma}{9} \\left(3 \\sin{\\left (\\phi_{1} + \\phi_{5} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{1} - \\phi_{5} \\right )}\\right), \\gamma \\left(\\cos{\\left (\\phi_{1} - \\phi_{6} \\right )} - 3 \\cos{\\left (\\phi_{1} + \\phi_{6} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+
+\\\\
+U_{2,j}, \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{2} \\right )} - 3 \\cos{\\left (\\phi_{0} + \\phi_{2} \\right )}\\right)
+ \\\\ \\qquad
+, \\gamma \\left(- 3 \\sin{\\left (\\phi_{1} + \\phi_{2} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{1} - \\phi_{2} \\right )}\\right), 0
+ \\\\ \\qquad
+, \\gamma \\left(\\cos{\\left (\\phi_{2} - \\phi_{3} \\right )} + 3 \\cos{\\left (\\phi_{2} + \\phi_{3} \\right )}\\right), \\frac{\\sqrt{3} \\gamma}{9} \\left(3 \\sin{\\left (\\phi_{2} + \\phi_{4} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{2} - \\phi_{4} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\gamma}{8} \\left(\\cos{\\left (\\phi_{2} - \\phi_{5} \\right )} - 3 \\cos{\\left (\\phi_{2} + \\phi_{5} + \\frac{\\pi}{3} \\right )}\\right), \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{2} - \\phi_{6} \\right )} - 3 \\cos{\\left (\\phi_{2} + \\phi_{6} \\right )}\\right)
+ \\\\ \\qquad
+
+\\\\
+U_{3,j}, \\gamma \\left(- 3 \\sin{\\left (\\phi_{0} + \\phi_{3} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{0} - \\phi_{3} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{1} - \\phi_{3} \\right )} + 3 \\cos{\\left (\\phi_{1} + \\phi_{3} + \\frac{\\pi}{3} \\right )}\\right), \\gamma \\left(\\cos{\\left (\\phi_{2} - \\phi_{3} \\right )} + 3 \\cos{\\left (\\phi_{2} + \\phi_{3} \\right )}\\right)
+ \\\\ \\qquad
+, 0, \\gamma \\left(\\cos{\\left (\\phi_{3} - \\phi_{4} \\right )} - 3 \\cos{\\left (\\phi_{3} + \\phi_{4} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{3} - \\phi_{5} \\right )} - 3 \\cos{\\left (\\phi_{3} + \\phi_{5} \\right )}\\right), \\frac{\\gamma}{8} \\left(- 3 \\sin{\\left (\\phi_{3} + \\phi_{6} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{3} - \\phi_{6} \\right )}\\right)
+ \\\\ \\qquad
+
+\\\\
+U_{4,j}, \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{4} \\right )} + 3 \\cos{\\left (\\phi_{0} + \\phi_{4} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\gamma}{8} \\left(\\cos{\\left (\\phi_{1} - \\phi_{4} \\right )} + 3 \\cos{\\left (\\phi_{1} + \\phi_{4} \\right )}\\right), \\frac{\\sqrt{3} \\gamma}{9} \\left(3 \\sin{\\left (\\phi_{2} + \\phi_{4} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{2} - \\phi_{4} \\right )}\\right)
+ \\\\ \\qquad
+, \\gamma \\left(\\cos{\\left (\\phi_{3} - \\phi_{4} \\right )} - 3 \\cos{\\left (\\phi_{3} + \\phi_{4} + \\frac{\\pi}{3} \\right )}\\right), 0
+ \\\\ \\qquad
+, \\gamma \\left(- 3 \\sin{\\left (\\phi_{4} + \\phi_{5} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{4} - \\phi_{5} \\right )}\\right), \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{4} - \\phi_{6} \\right )} + 3 \\cos{\\left (\\phi_{4} + \\phi_{6} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+
+\\\\
+U_{5,j}, \\gamma \\left(\\cos{\\left (\\phi_{0} - \\phi_{5} \\right )} + 3 \\cos{\\left (\\phi_{0} + \\phi_{5} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\sqrt{3} \\gamma}{9} \\left(3 \\sin{\\left (\\phi_{1} + \\phi_{5} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{1} - \\phi_{5} \\right )}\\right), \\frac{\\gamma}{8} \\left(\\cos{\\left (\\phi_{2} - \\phi_{5} \\right )} - 3 \\cos{\\left (\\phi_{2} + \\phi_{5} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{3} - \\phi_{5} \\right )} - 3 \\cos{\\left (\\phi_{3} + \\phi_{5} \\right )}\\right), \\gamma \\left(- 3 \\sin{\\left (\\phi_{4} + \\phi_{5} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{4} - \\phi_{5} \\right )}\\right)
+ \\\\ \\qquad
+, 0, \\gamma \\left(\\cos{\\left (\\phi_{5} - \\phi_{6} \\right )} + 3 \\cos{\\left (\\phi_{5} + \\phi_{6} \\right )}\\right)
+ \\\\ \\qquad
+
+\\\\
+U_{6,j}, \\gamma \\left(3 \\sin{\\left (\\phi_{0} + \\phi_{6} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{0} - \\phi_{6} \\right )}\\right)
+ \\\\ \\qquad
+, \\gamma \\left(\\cos{\\left (\\phi_{1} - \\phi_{6} \\right )} - 3 \\cos{\\left (\\phi_{1} + \\phi_{6} + \\frac{\\pi}{3} \\right )}\\right), \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{2} - \\phi_{6} \\right )} - 3 \\cos{\\left (\\phi_{2} + \\phi_{6} \\right )}\\right)
+ \\\\ \\qquad
+, \\frac{\\gamma}{8} \\left(- 3 \\sin{\\left (\\phi_{3} + \\phi_{6} + \\frac{\\pi}{6} \\right )} + \\cos{\\left (\\phi_{3} - \\phi_{6} \\right )}\\right), \\frac{\\sqrt{3} \\gamma}{9} \\left(\\cos{\\left (\\phi_{4} - \\phi_{6} \\right )} + 3 \\cos{\\left (\\phi_{4} + \\phi_{6} + \\frac{\\pi}{3} \\right )}\\right)
+ \\\\ \\qquad
+, \\gamma \\left(\\cos{\\left (\\phi_{5} - \\phi_{6} \\right )} + 3 \\cos{\\left (\\phi_{5} + \\phi_{6} \\right )}\\right), 0
+ \\\\ \\qquad      `,
+      explanation: "final potential"
     },
     {
       expression: `NA`,
